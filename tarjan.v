0b954bf59739c33e082a39cf12b8b90da00d60eb
Cyril Cohen <cohen@crans.org>
2017-03-09 15:06:24 +0100

    Initial Import

---
+++ tarjan.v
@@ -0,0 +1,249 @@
+From mathcomp
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq choice fintype tuple.
+From mathcomp
+Require Import bigop finset finfun perm fingraph path.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section tarjan.
+Variable (V : finType) (g : V -> seq V).
+Notation gconnect := (connect (grel g)).
+Notation infty := #|V|.
+
+Definition split_after (T :eqType) (x : T) (s : seq T) :=
+  let i := index x s in (rcons (take i s) x, drop i.+1 s).
+
+Fixpoint rank (x : V) stack : nat :=
+  if stack isn't y :: s then infty
+  else if (x == y) && (x \notin s) then size s else rank x s.
+
+Lemma rankE x stack :
+  rank x stack = if x \in stack then index x (rev stack) else infty.
+Proof.
+elim: stack => [|a s /= ->] //.
+have [->|neq_xa] /= := altP eqP; rewrite rev_cons -cats1.
+  by rewrite mem_head index_cat mem_rev /= eqxx addn0 size_rev; case: in_mem.
+by rewrite in_cons (negPf neq_xa) /= index_cat /= mem_rev; case: in_mem.
+Qed.
+
+Arguments rank : simpl never.
+
+Record env := Env {blacks : {set V}; stack : seq V; sccs : {set {set V}}}.
+Definition grays (e : env) := [set x in stack e] :\: blacks e.
+Definition whites (e : env) := ~: (grays e :|: blacks e).
+
+Definition add_stack x e := Env (blacks e) (x :: stack e) (sccs e).
+Definition add_blacks x e := Env (x |: blacks e) (stack e) (sccs e).
+Definition add_sccs x e := let (s2, s3) := split_after x (stack e) in
+                            Env (x |: blacks e) s3 ([set y in s2] |: sccs e).
+
+Definition dfs1 (dfs' : {set V} -> env -> nat * env) (x : V) e :=
+    let m := rank x (x :: stack e) in
+    let: (m1, e1) := dfs' [set y in g x] (add_stack x e) in
+    if m1 >= m then (infty, add_sccs x e1) else (m1, add_blacks x e1).
+
+Definition dfs' dfs1 dfs' (roots : {set V}) e :=
+  if [pick x in roots] isn't Some x then (infty, e)
+  else let roots' := roots :\ x in
+       let: (m1, e1) :=
+         if x \in stack e then (rank x (stack e), e)
+         else if x \in blacks e then (infty, e)
+         else dfs1 x e in
+       let: (m2, e2) := dfs' roots' e1 in (min m1 m2, e2).
+
+Fixpoint tarjan n : {set V} -> env -> nat * env :=
+  if n is n.+1 then dfs' (dfs1 (tarjan n)) (tarjan n)
+  else fun r e => (infty, e).
+
+Definition gsccs := [set [set y in V | gconnect x y && gconnect y x] | x in V].
+
+Definition noblack_to_white e := [forall x in blacks e, [disjoint g x & whites e]].
+
+Definition wf_env e := 
+  [/\ [set x in stack e] = grays e :|: (blacks e :\: \bigcup_(scc in sccs e) scc),
+   \bigcup_(scc in sccs e) scc \subset blacks e &
+   uniq (stack e)].
+
+Inductive color_spec x e : bool -> bool -> bool -> bool -> bool -> Type :=
+| ColorGray of x \in grays e : color_spec x e false true false true false
+| ColorSccs of x \in \bigcup_(scc in sccs e) scc :
+                       color_spec x e true false true false false
+| ColorWhite of x \in whites e : color_spec x e false false false false true
+| ColorBlackStack of x \in blacks e & x \in stack e :
+                        color_spec x e true true false false false.
+
+Lemma colorP x e : wf_env e -> 
+                   color_spec x e (x \in blacks e) (x \in stack e) 
+                              (x \in \bigcup_(scc in sccs e) scc)
+                              (x \in grays e) (x \in whites e).
+Proof.
+move=> [/setP /(_ x) s_def] /subsetP /(_ x) /implyP.
+move: s_def; rewrite /grays /whites !inE.
+case x_black: (_ \in blacks _) => /=;
+case x_stack : (_ \in stack _) => /=;
+case x_sccs : (_ \in \bigcup_(_ in _) _) => //=; do ?by constructor.
+  by constructor=> //; rewrite /grays !inE x_black.
+by constructor; rewrite /whites !inE x_black x_stack.
+Qed.
+
+Lemma whites_blacksF x e : x \in whites e -> x \in blacks e = false.
+Proof. by rewrite !inE; case: (x \in blacks _). Qed.
+
+Lemma whites_stackF x e : x \in whites e -> x \in stack e = false.
+Proof. by rewrite !inE andbC; case: (x \in stack _); rewrite //= orNb. Qed.
+
+Lemma grays_stack x e : x \in grays e -> x \in stack e.
+Proof. by rewrite !inE andbC; case: (x \in stack _). Qed.
+
+Lemma grays_sccsF x e : wf_env e -> x \in grays e -> x \in \bigcup_(scc in sccs e) scc = false.
+Proof. by move=> /colorP []. Qed.
+
+Lemma add_stack_wf x e : x \in whites e -> wf_env e -> wf_env (add_stack x e).
+Proof.
+move=> x_white [s_def sccs_blacks s_uniq]; split => //=;
+  last by rewrite whites_stackF.
+apply/setP=> y; rewrite !inE.
+by have [->|] := altP (y =P x); case: colorP=> //=; case: colorP x_white.
+Qed.
+
+Lemma add_blacks_wf x e : x \in grays e -> wf_env e -> wf_env (add_blacks x e).
+Proof.
+move=> x_gray [s_def sccs_blacks s_uniq]; split => //=; last first.
+  by rewrite subsetU // sccs_blacks orbT.
+apply/setP=> y; rewrite !inE.
+by have [->|] := altP (y =P x); case: colorP=> //=; case: colorP x_gray.
+Qed.
+
+Lemma drop_subseq (T : eqType) n (s : seq T) : subseq (drop n s) s.
+Proof. by rewrite -[X in subseq _ X](cat_take_drop n) suffix_subseq. Qed.
+
+Lemma add_sccs_wf x e : 
+  take (index x (stack e)) (stack e) \subset blacks e ->
+  x \in grays e -> wf_env e -> wf_env (add_sccs x e).
+Proof.
+move=> /subsetP new_blacks x_gray e_wf.
+have [s_def /subsetP sccs_blacks s_uniq] := e_wf.
+split => //=; last first.
+- by rewrite (subseq_uniq (drop_subseq _ _)).
+- apply/subsetP=> y; rewrite !inE.
+  rewrite bigcup_setU inE big_set1 !inE /= => /orP[|/sccs_blacks->];
+    last by rewrite !orbT.
+  by rewrite mem_rcons !inE; case: eqP => //= _ /new_blacks.
+apply/setP=> y; rewrite !inE bigcup_setU inE big_set1 !inE !negb_or.
+have [->|neq_xy] //= := altP (y =P x); rewrite ?(andbT, andbF).
+  case: splitP new_blacks s_uniq => //=; first by rewrite grays_stack.
+  move=> s1 s2 s1x_blacks s_uniq; rewrite grays_sccsF // andbT.
+  have := s_uniq; rewrite cat_uniq => /and3P [_ /hasPn /(_ x) /=].
+  by rewrite mem_rcons mem_head; case: (_ \in _) => //= /(_ isT).
+case: colorP; rewrite ?(andbT, andbF, orbT, orbF) //=.
+  by move=> y_sccs; apply: contraTF y_sccs => /mem_drop; case: colorP.
+move=> y_blacks; case: splitP s_uniq; first by rewrite grays_stack.
+move=> s1 s2 s_uniq; rewrite mem_cat => /orP[y_s1x|y_s2].
+- move: s_uniq; rewrite uniq_catC cat_uniq => /and3P [_ /hasPn /(_ _ y_s1x) /=].
+  by move=> /negPf->; rewrite y_s1x.
+- move: s_uniq; rewrite cat_uniq => /and3P [_ /hasPn /(_ _ y_s2) /=].
+  by move=> /negPf->; rewrite y_s2.
+Qed.
+
+
+
+Definition wf_color e :=
+  [/\ [set x in stack e] = grays e :|: (blacks e :\: \bigcup_(scc in sccs e) scc),
+   \bigcup_(scc in sccs e) scc \subset blacks e &
+   noblack_to_white e].
+
+Definition wf_stack e :=
+  [/\ wf_color e,
+      uniq (stack e),
+      forall x, x \in grays e -> forall y, y \in stack e ->
+          (rank x (stack e) <= rank y (stack e)) -> gconnect x y &
+      forall y, y \in stack e -> exists2 x, x \in grays e &
+          (rank x (stack e) <= rank y (stack e)) && gconnect y x
+  ].
+
+
+
+Definition black_gsccs e := [set scc in gsccs | scc \subset blacks e].
+
+Definition xedges (new old : seq V) :=
+  [set y in old | [exists x in new, (x \notin old) && grel g x y]].
+
+Definition dfs_correct (roots : {set V}) (e e' : env) (m : nat) :=
+  [/\ (forall x, x \in roots -> forall y, y \in grays e -> gconnect y x),
+    wf_stack e & sccs e == black_gsccs e]
+
+  ->
+
+  [/\ (wf_stack e' /\ (sccs e' == black_gsccs e')),
+
+   [/\ exists x, let: (new, old) := split_after x (stack e) in
+          (old == stack e) /\ (new \subset blacks e),
+    blacks e \subset blacks e'  & sccs e \subset sccs e' ],
+
+   (roots :&: whites e \subset blacks e') &
+
+   [/\ roots \subset blacks e' :|: grays e',
+    forall x, x \in roots -> m <= rank x (stack e'),
+    forall x, x \in roots -> forall y, y \in gconnect x ->
+         m != rank y (stack e') -> (m == infty) &
+    forall y, y \in xedges (stack e') (stack e) -> m <= rank y (stack e')
+   ]
+  ].
+
+Definition dfs1_correct (dfs1 : V -> env -> nat * env) x e :=
+  let (m, e') := dfs1 x e in (x \in whites e) -> dfs_correct (set1 x) e e' m.
+
+Definition dfs'_correct (dfs' : {set V} -> env -> nat * env) roots e :=
+  let (m, e') := dfs' roots e in dfs_correct roots e e' m.
+
+Lemma dfs1_is_correct (dfs' : {set V} -> env -> nat * env) (x : V) e :
+  (dfs'_correct dfs' [set y in g x] (add_stack x e)) ->
+  dfs1_correct (dfs1 dfs') x e.
+Proof.
+rewrite /dfs1_correct /dfs1 /=.
+rewrite /dfs'_correct.
+case: (dfs' _ _) => m e' /= dfs_is_correct.
+case: leqP => [rank_small|rank_big] /=; case: e' => b' s' sccs' in dfs_is_correct *.
+  move=> x_white []; split.
+  - split.
+    + split.
+      * split => /=.
+        rewrite /grays /=.
+        
+   
+  
+move=> 
+
+
+
+Admitted.
+  
+Lemma dfs'_is_correct dfs1 subdfs' (roots : {set V}) e :
+  (forall x, x \in roots -> dfs1_correct dfs1 x e) ->
+  (forall x, x \in roots -> dfs'_correct subdfs' (roots :\ x) e) ->
+  dfs'_correct (dfs' dfs1 subdfs') roots e.
+Proof.
+Admitted.
+
+Theorem tarjan_is_correct : forall n, n >= #|V| ->
+  tarjan n setT (Env set0 [::] set0) = (infty, Env setT [::] gsccs).
+Proof.
+
+move=> n n_geV.
+elim: n n_geV => /= [|n IHn].
+  rewrite leqn0 -cardsT cards_eq0 => /eqP V_eq0; rewrite V_eq0.
+(*   suff -> : sccs = set0 by []. *)
+(*   apply: contraTeq isT => /set0Pn [x] /imsetP [i]. *)
+(*   by have := in_setT i; rewrite V_eq0 inE. *)
+(* move=> Sn_geV; rewrite /dfs'. *)
+(* have [x xV /=|/= ?] := pickP; last first. *)
+(*   admit. *)
+Abort.
+
+
+
+
+
+End tarjan.
