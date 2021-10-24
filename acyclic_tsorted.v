aea92829eb5c42071618149f65e86e96df919d63
Karl Palmskog <palmskog@gmail.com>
2021-10-24 14:21:29 +0200

    reorganize final results on tsorting

---
+++ acyclic_tsorted.v
@@ -0,0 +1,94 @@
+From mathcomp Require Import all_ssreflect.
+Require Import extra acyclic Kosaraju.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section AcyclicTSorted.
+
+Variable V : finType.
+Variable g : rel V.
+
+Hypothesis acyclicg : acyclic g.
+
+Variable vs : seq V.
+
+Hypothesis vs_all : forall x, x \in vs.
+Hypothesis vs_tsorted : tsorted g (pred_of_simpl predT) vs.
+
+Lemma tsorted_connect_before_nth x y :
+  connect g x y ->
+  before vs (nth x vs (find (symconnect g x) vs)) y.
+Proof. by move: (vs_all x); apply vs_tsorted. Qed.
+
+Lemma in_nth_find_symconnect s x :
+  x \in s -> nth x s (find (symconnect g x) s) = x.
+Proof.
+elim: s x => //= y s IH x.
+rewrite in_cons; move/orP; case.
+  move/eqP =>->; case: ifP => //=; move/negP.
+  by case; apply: symconnect0.
+case xy: (x == y).
+  move/eqP: xy =>->; case: ifP => // /negP.
+  by case; apply: symconnect0.
+move/negP/negP: xy => xneqy xs; case: ifP; last first.
+  by move => symcgxy; apply: IH.
+move=> symcxy; move/negP/negP/eqP: xneqy; case. 
+by move: symcxy; apply: acyclic_symconnect.
+Qed.
+
+Lemma acyclic_connect_before x y :
+  connect g x y -> before vs x y.
+Proof.
+move => cgxy; move: (tsorted_connect_before_nth cgxy).
+by rewrite in_nth_find_symconnect.
+Qed.
+
+End AcyclicTSorted.
+
+Section AcyclicTseq.
+
+Variable V : finType.
+
+Lemma tseq_connect_before (g : V -> seq V) x y :
+  acyclic (grel g) -> connect (grel g) x y ->
+  before (tseq g) x y.
+Proof.
+move: (tseq_correct g) => [sorted all] acycg.
+apply: acyclic_connect_before.
+- exact: acycg.
+- exact: all.
+- exact: sorted.
+Qed.
+
+Lemma tseq_rel_connect_before (g : rel V) x y :
+  acyclic g -> connect g x y ->
+  before (tseq (rgraph g)) x y.
+Proof.
+move => acycg cgxy.
+apply: tseq_connect_before.
+  apply (@eq_acyclic _ g) => // x0 y0.
+  by rewrite /grel /rgraph /= mem_enum.
+rewrite -(@eq_connect _ g) // => x0 y0.
+by rewrite /grel /rgraph /= mem_enum.
+Qed.
+
+End AcyclicTseq.
+
+Section AcyclicKosaraju.
+
+Variable V : finType.
+Variable g : rel V.
+
+Definition kosaraju_acyclic :=
+  sccs_acyclic (@kosaraju V) g.
+
+Lemma kosaraju_acyclicP :
+  reflect (acyclic g) kosaraju_acyclic.
+Proof.
+case: (kosaraju_correct g) => Hu Hf Hc.
+exact: sccs_acyclicP.
+Qed.
+
+End AcyclicKosaraju.
